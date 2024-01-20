0ffae2ff30367ed090a65e54ad99773bb6e1c8af
James-Muthama <jamesmuthaiks@gmail.com>
2024-01-20 05:02:49 +0300

    The training of the model

---
+++ CustomerInsightAI/AI_categoriser/training_the_model.py
@@ -0,0 +1,23 @@
+from CustomerInsightAI.AI_categoriser.creating_model import model
+from CustomerInsightAI.AI_categoriser.preprocessing_json_file import training
+from CustomerInsightAI.AI_categoriser.preprocessing_json_file import output
+from tensorflow.keras.callbacks import TensorBoard, EarlyStopping
+
+
+# tries to load the model if it is not available we will need to train it
+try:
+    model.load("CustomerInsightAI.tflearn")
+
+except:
+    # Define a TensorBoard callback for visualizing metrics
+    tensorboard_callback = TensorBoard(log_dir="./logs", histogram_freq=1)
+
+    # Define an EarlyStopping callback to prevent overfitting
+    early_stopping_callback = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
+
+    # fitting the neural network with the training data, output, number of epochs, batch size and where it will show
+    # metrics such as accuracy etc
+    model.fit(training, output, epochs=150, batch_size=16, validation_split=0.2, callbacks=[tensorboard_callback, early_stopping_callback])
+
+    # saving the model as SmartNannyBot.tflearn
+    model.save("CustomerInsightAI.tflearn")
