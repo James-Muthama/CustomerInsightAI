a5b33e5af028a2783bc7baf7c3c98bd67f033578
James-Muthama <jamesmuthaiks@gmail.com>
2024-01-24 16:31:19 +0300

    Reduced the number of epochs from 80 to 75

--- CustomerInsightAI/AI_categoriser/training_the_model.py
+++ CustomerInsightAI/AI_categoriser/training_the_model.py
@@ -2,11 +2,11 @@ from CustomerInsightAI.AI_categoriser.creating_model import model
 from CustomerInsightAI.AI_categoriser.preprocessing_json_file import training
 from CustomerInsightAI.AI_categoriser.preprocessing_json_file import output
 from tensorflow.keras.callbacks import TensorBoard, EarlyStopping
-
+from tensorflow.keras.models import load_model
 
 # tries to load the model if it is not available we will need to train it
 try:
-    model.load("CustomerInsightAI.tflearn")
+    model = load_model("CustomerInsightAI.tflearn")
 
 except:
     # Define a TensorBoard callback for visualizing metrics
@@ -17,7 +17,8 @@ except:
 
     # fitting the neural network with the training data, output, number of epochs, batch size and where it will show
     # metrics such as accuracy etc
-    model.fit(training, output, epochs=75, batch_size=20, validation_split=0.2, callbacks=[tensorboard_callback, early_stopping_callback])
+    model.fit(training, output, epochs=75, batch_size=20, validation_split=0.2,
+              callbacks=[tensorboard_callback, early_stopping_callback])
 
     # saving the model as SmartNannyBot.tflearn
     model.save("CustomerInsightAI.tflearn")
