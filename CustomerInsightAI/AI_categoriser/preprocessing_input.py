f74dece66c5ea7564428bddb9839dfe6c7a0956d
James-Muthama <jamesmuthaiks@gmail.com>
2024-01-24 16:30:46 +0300

    Modified the function bag_of_words which will now return a 2D numpy array instead of 1D numpy array

--- CustomerInsightAI/AI_categoriser/preprocessing_input.py
+++ CustomerInsightAI/AI_categoriser/preprocessing_input.py
@@ -6,20 +6,18 @@ from nltk.stem.lancaster import LancasterStemmer
 stemmer = LancasterStemmer()
 
 
-# create a function that takes in input from a user converts it into a numpy array to feed into the model
-def bag_of_words(s, words):
-    # creates the bag of words
-    bag = [0 for _ in range(len(words))]
+def bag_of_words(sentence, words):
+    # Tokenize and stem the input sentence
+    tokenized_words = nltk.word_tokenize(sentence)
+    stemmed_words = [stemmer.stem(word.lower()) for word in tokenized_words]
 
-    # tokenizes every word inputted by the user and stems them
-    s_words = nltk.word_tokenize(s)
-    s_words = [stemmer.stem(word.lower()) for word in s_words]
+    # Create the bag of words
+    bag = [0] * len(words)
 
-    # fills the bag of words with 1 if a stemmed word matches a word from the words list
-    for se in s_words:
-        for i, w in enumerate(words):
-            if w == se:
-                bag[i] = 1
+    # Fill the bag of words
+    for i, w in enumerate(words):
+        if w in stemmed_words:
+            bag[i] = 1
 
-    # returns a numpy array to be passed in the model
-    return np.array(bag)
+    # Convert the bag to a NumPy array
+    return np.array([bag])
