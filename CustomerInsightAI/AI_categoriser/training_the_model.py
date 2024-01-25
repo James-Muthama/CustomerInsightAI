6dfebc1b970ccf06eb21af98f564a07a078eca59
James-Muthama <jamesmuthaiks@gmail.com>
2024-01-25 05:13:21 +0300

    modified training_the_model.py and added a function that trains the model to allow it to be called when the model fails to load

--- CustomerInsightAI/AI_categoriser/training_the_model.py
+++ CustomerInsightAI/AI_categoriser/training_the_model.py
@@ -2,13 +2,9 @@ from CustomerInsightAI.AI_categoriser.creating_model import model
 from CustomerInsightAI.AI_categoriser.preprocessing_json_file import training
 from CustomerInsightAI.AI_categoriser.preprocessing_json_file import output
 from tensorflow.keras.callbacks import TensorBoard, EarlyStopping
-from tensorflow.keras.models import load_model
 
-# tries to load the model if it is not available we will need to train it
-try:
-    model = load_model("CustomerInsightAI.tflearn")
 
-except:
+def train_model():
     # Define a TensorBoard callback for visualizing metrics
     tensorboard_callback = TensorBoard(log_dir="./logs", histogram_freq=1)
 
@@ -17,8 +13,8 @@ except:
 
     # fitting the neural network with the training data, output, number of epochs, batch size and where it will show
     # metrics such as accuracy etc
-    model.fit(training, output, epochs=75, batch_size=20, validation_split=0.2,
-              callbacks=[tensorboard_callback, early_stopping_callback])
+    model.fit(training, output, epochs=75, batch_size=20, validation_split=0.2, callbacks=[tensorboard_callback, early_stopping_callback])
 
     # saving the model as SmartNannyBot.tflearn
     model.save("CustomerInsightAI.tflearn")
+
