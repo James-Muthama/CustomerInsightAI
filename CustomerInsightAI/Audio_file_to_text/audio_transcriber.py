4a584a2d0552602228b4272224a86fe691654176
James-Muthama <jamesmuthaiks@gmail.com>
2024-01-25 05:09:47 +0300

    modified the audio_transcribe function by calling the ffmpeg.exe file

--- CustomerInsightAI/Audio_file_to_text/audio_transcriber.py
+++ CustomerInsightAI/Audio_file_to_text/audio_transcriber.py
@@ -1,13 +1,20 @@
 import whisper
+import os
 
 
-def audio_transcription(audio_file):
+def audio_transcription(audio_file_path):
+    # Specify the full path to the ffmpeg executable
+    ffmpeg_path = 'C:/Users/James Muthama/PycharmProjects/ABSAproject/CustomerInsightAI/Audio_file_to_text/ffmpeg.exe'
+
+    # Set the environment variable for ffmpeg
+    os.environ['PATH'] = f"{os.environ['PATH']};{os.path.dirname(ffmpeg_path)}"
+
     # Load the "base" model from the whisper library
     model = whisper.load_model("base")
 
     # Perform audio transcription using the loaded model
     # fp16=False specifies not to use half-precision (FP16) floating-point format
-    result = model.transcribe(audio_file, fp16=False)
+    result = model.transcribe(audio_file_path, fp16=False)
 
     # Retrieve the transcribed text from the result dictionary
     transcribed_text = result["text"]
